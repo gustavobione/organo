25d9fd65c9ed207cb58b52f1ec4ee6d0a492fa7a
Gustavo <gustavoteixeira394@gmail.com>
2025-01-20 14:25:35 -0300

    Criação do Componente Formulário

--- src/App.js
+++ src/App.js
@@ -1,15 +1,13 @@
 import Banner from './Componentes/Banner';
-import CampoTexto from './Componentes/CampoTexto';
+import Formulario from './Componentes/Formulario';
 
 function App() {
-  return (
-    <div className="App">
-      < Banner />
-      <CampoTexto label="Nome"/>
-      <CampoTexto label="Cargo"/>
-      <CampoTexto label="Imagem"/>
-    </div>
-  );
+    return (
+        <div className="App">
+            <Banner />
+            <Formulario />
+        </div>
+    )
 }
 
 export default App;
