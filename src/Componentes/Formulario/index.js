9d77a4374d622fee34099002a7327475f0eb7601
Gustavo <gustavoteixeira394@gmail.com>
2025-01-21 15:38:51 -0300

    Criação da Lista Suspensa

--- src/Componentes/Formulario/index.js
+++ src/Componentes/Formulario/index.js
@@ -1,7 +1,19 @@
 import CampoTexto from '../CampoTexto';
 import './Formulario.css';
+import ListaSuspensa from '../ListaSuspensa';
 
 const Formulario = () => {
+
+    const times = [
+        'Programação',
+        'Front-End',
+        'Data Science',
+        'Devops',
+        'UX e Design',
+        'Mobile',
+        ' Inovação e Gestão'
+        ]
+    
     return (
         <section>
             <form>
@@ -9,6 +21,7 @@ const Formulario = () => {
                 <CampoTexto label="Nome" placeholder="Digite seu nome"/>
                 <CampoTexto label="Cargo" placeholder="Digite seu cargo"/>
                 <CampoTexto label="Imagem" placeholder="Digite o endereço da imagem"/>
+                <ListaSuspensa label="Time" itens={times}/>
             </form>
         </section>
     )
