dca8c2fc3251066ecfa027f088bf4709552e118d
Gustavo <gustavoteixeira394@gmail.com>
2025-01-23 17:00:43 -0300

    Card Colaborador

---
+++ src/Componentes/Colaborador/index.js
@@ -0,0 +1,17 @@
+import './Colaborador.css';
+
+const Colaborador = () => {
+    return (
+    <div className='colaborador'>
+        <div className='cabecalho'>
+            <img scr='https://github.com/viniciosneves.png' alt=''/>
+        </div>
+        <div className='rodape'>
+            <h4>Vinicios Neves</h4>
+            <h5>Instrutor</h5>
+        </div>
+    </div>
+    )
+}
+
+export default Colaborador
