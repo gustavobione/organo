1fad78984c64f9ff10f8f9f855f96a77728dd7b3
Gustavo <gustavoteixeira394@gmail.com>
2025-01-27 15:37:15 -0300

    Mapeamento de colaboradores e utlização de props

--- src/Componentes/Colaborador/index.js
+++ src/Componentes/Colaborador/index.js
@@ -1,14 +1,14 @@
 import './Colaborador.css';
 
-const Colaborador = () => {
+const Colaborador = (props) => {
     return (
     <div className='colaborador'>
         <div className='cabecalho'>
-            <img src="/imagens/imagem-teste.png" alt=''/>
+            <img src={props.imagem} alt={props.nome}/>
         </div>
         <div className='rodape'>
-            <h4>Vinicios Neves</h4>
-            <h5>Instrutor</h5>
+            <h4>{props.nome}</h4>
+            <h5>{props.cargo}</h5>
         </div>
     </div>
     )
