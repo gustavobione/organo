cf095bba95e874b480076ed39eb6d4480f5fe5ca
Gustavo <gustavoteixeira394@gmail.com>
2025-01-27 16:02:49 -0300

    Exclusão de espaços vazios

--- src/Componentes/Formulario/index.js
+++ src/Componentes/Formulario/index.js
@@ -18,6 +18,10 @@ const Formulario = (props) => {
             imagem: imagem,
             time: time
         });
+        setNome('')
+        setCargo('')
+        setImagem('')
+        setTime('')
     }
 
     return (
